1bb075645cca88bb51b998b33a760dc80facf22c
Júlio César Melo <jcesarsm17@gmail.com>
2023-10-27 13:06:52 -0300

    ==public

--- public/view/tela_login.php
+++ public/view/tela_login.php
@@ -20,16 +20,16 @@
                 <hr>
             </div>
         </div>
-        <form action="../app/validação_login.php" method="POST">
+        <form action="../app/logar.php" method="POST">
             <div class="row justify-content-center">
                 <div class="col-4">
                     <div class="form-group mb-2">
-                        <label for="nome">Nome</label>
-                        <input type="text" name="nome" id="nome" class="form-control">
+                        <label for="email">E-mail</label>
+                        <input type="text" name="email" id="email" class="form-control">
                     </div>
                     <div class="form-group mb-3">
-                        <label for="cpf">CPF</label>
-                        <input type="text" name="cpf" id="cpf" class="form-control" placeholder="xxx.xxx.xxx-xx" maxlength="14">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" id="senha" class="form-control">
                     </div>
 
                     <button type="submit" class="btn btn-primary">Entrar</button>
