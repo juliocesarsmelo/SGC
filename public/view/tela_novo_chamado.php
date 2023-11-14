bf593c998d8b4bcec790dfe3b03a41cd5de59834
Júlio César Melo <jcesarsm17@gmail.com>
2023-11-14 11:19:31 -0300

    ==view + ajustando title das páginas

--- public/view/tela_novo_chamado.php
+++ public/view/tela_novo_chamado.php
@@ -15,7 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novo Chamado | SGC</title>
+    <title>SGC | Novo Chamado</title>
 </head>
 <body>
     
--- public/view/tela_principal.php
+++ public/view/tela_principal.php
@@ -15,7 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | SGC</title>
+    <title>SGC | Home</title>
 </head>
 <body>
     
