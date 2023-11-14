003354f853becb59f1f0224035dfb797c7974b7e
Júlio César Melo <jcesarsm17@gmail.com>
2023-11-14 12:42:35 -0300

    ==logout.php + ajustando logout

--- app/controller/logout.php
+++ app/controller/logout.php
@@ -1,11 +1,19 @@
 <?php 
 
-    //  Iniciando sessão ou resumindo sessão existe  
-    session_start();  
+session_start(); 
 
-    // .............................................................. //
-    //  Fazendo Logout no sistema                                     //
-    // .............................................................. //
-    session_destroy();
-    
-    header("Location: ../index.php");
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy(); 
+
+header("location: ../../index.php"); 
+
+?>
